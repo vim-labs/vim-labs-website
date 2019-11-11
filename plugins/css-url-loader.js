521fca947dd9aed46f4d6c6fb4d69f976c146f45
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2019-11-11 00:08:30 -0800

    Formatting

--- plugins/css-url-loader.js
+++ plugins/css-url-loader.js
@@ -1,18 +1,14 @@
-module.exports = function(context, options) {
-  return {
-    name: "css-url-loader",
-    configureWebpack() {
-      return {
-        mode: process.env.NODE_ENV || "development",
-        module: {
-          rules: [
-            {
-              test: /\.(gif|png|jpe?g|svg)$/i,
-              use: ["url-loader"]
-            }
-          ]
+module.exports = () => ({
+  name: "css-url-loader",
+  configureWebpack: () => ({
+    mode: process.env.NODE_ENV || "development",
+    module: {
+      rules: [
+        {
+          test: /\.(gif|png|jpe?g|svg)$/i,
+          use: ["url-loader"]
         }
-      };
+      ]
     }
-  };
-};
+  })
+});
