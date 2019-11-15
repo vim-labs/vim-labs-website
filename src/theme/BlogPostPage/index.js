193e8180ddc1bef05c32430ba385dcd1128c51cb
Ryan Bell <25379378+iRyanBell@users.noreply.github.com>
2019-11-14 20:07:05 -0800

    Fix: Frontmatter featuredImage metadata

--- src/theme/BlogPostPage/index.js
+++ src/theme/BlogPostPage/index.js
@@ -20,7 +20,7 @@ function BlogPostPage(props) {
     <Layout
       title={`${siteConfig.title} | ${metadata.title}`}
       description={metadata.description}
-      image={metadata.featuredImage}
+      image={frontMatter.featuredImage}
     >
       {BlogPostContents && (
         <div className="container margin-vert--xl">
